3cd0cb2f4fb3380871f5b974b55b8840d0133f38
dviejo <dvp123456@gmail.com>
2019-01-27 11:55:45 +0100

    deploy to now

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(metricsMiddleware)
 app.get("/ping", (req, res) => {
   res.send({ message: "OK" })
 })
-const server = app.listen(process.env.PORT || 3000, () => {
-  const { port, address } = server.address()
-  console.log(`Listening on ${address}${port}`)
+const port = process.env.PORT || 3000
+
+app.listen(port, err => {
+  if (err) throw err
+  console.log(`> Ready On Server http://localhost:${port}`)
 })
